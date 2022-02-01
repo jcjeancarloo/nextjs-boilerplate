a0ed3c09a591f6e01655561d8759026c7cda3284
Jean Carlo <jcjeancarloo@gmail.com>
2022-02-01 19:55:58 -0300

    jest with styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>Next Boilerplate</title>
         {/* <link rel="shorcut icon" href="" /> */}
         {/* <link rel="apple-touch-icon" href=""/> */}
+        {/* <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" /> */}
         <meta
           name="description"
           content="A simple project start to work with React, Typescript, NextJS and Styled Components"
